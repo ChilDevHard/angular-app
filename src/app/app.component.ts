cdff96a59da55fe7af00f799f3f026150ca58139
IL S <nicechildev@gmail.com>
2025-04-06 20:04:20 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeBarComponent } from './welcome-bar/welcome-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    WelcomeBarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/welcome-bar/welcome-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-bar',
+  imports: [],
+  templateUrl: './welcome-bar.component.html',
+  styleUrls: ['./welcome-bar.component.css']
+})
+export class WelcomeBarComponent {
+
+}
